acc9723c604cf4449e9e134c1852e61f6647a88c
Exceret <15364051195@163.com>
2026-02-12 15:02:55 +0800

    update ts_print

--- inst/python/73-CellTypistAnnotate.py
+++ inst/python/73-CellTypistAnnotate.py
@@ -64,19 +64,21 @@ def ts_print(
     timestamp = datetime.now().strftime("[%Y/%m/%d %H:%M:%S]")
 
     symbols = {
-        "info": ("💡", "\033[36m" if color else ""),  # Cyan
-        "success": ("✅", "\033[32m" if color else ""),  # Green
-        "warning": ("⚠️", "\033[33m" if color else ""),  # Yellow
-        "error": ("❌", "\033[31m" if color else ""),  # Red
-        "debug": ("🐞", "\033[35m" if color else ""),  # Magenta
+        "info": ("ℹ", "\033[36m" if color else ""),  # Cyan
+        "success": ("✔", "\033[32m" if color else ""),  # Green
+        "warning": ("⚠", "\033[33m" if color else ""),  # Yellow
+        "error": ("✖", "\033[31m" if color else ""),  # Red
+        "debug": ("◼", "\033[35m" if color else ""),  # Magenta
     }
 
-    prefix: str = f"{timestamp} "
+    t_prefix: str = f"{timestamp} "
     if symbol in symbols:
         sym, col = symbols[symbol]
-        prefix += f"{col}{sym} \033[0m" if color else f"{sym} "
+        symbol_prefix: str = f"{col}{sym} \033[0m" if color else f"{sym} "
+    else:
+        symbol_prefix: str = ""
 
-    print(f"{prefix}{message}" if not color else f"{prefix}\033[0m{message}")
+    print(symbol_prefix + f"{t_prefix}{message}")
 
 
 def filter_args_4_func(input_dict: dict[str, Any], func: Callable) -> dict[str, Any]:
