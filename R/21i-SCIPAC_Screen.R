d517ad90f6bb6bd39fdd9016380c947a9ea120e4
Exceret <15364051195@163.com>
2026-04-10 15:57:37 +0800

    fix wrong argument check

--- R/21i-SCIPAC_Screen.R
+++ R/21i-SCIPAC_Screen.R
@@ -187,13 +187,14 @@ DoSCIPAC <- function(
   CheckInstalled("Exceret/SCIPAC")
   # Validate phenotype_class parameter
   purrr::walk(
-    .x = c(hvg, n_pc, resolution, bt_size, ncore, nfold),
+    .x = c(hvg, n_pc, bt_size, ncore, nfold),
     .f = chk::chk_integer,
   )
   purrr::walk(
     .x = c(ela_net_alpha, ci_alpha),
     .f = chk::chk_numeric,
   )
+  chk::chk_numeric(resolution)
   chk::chk_chr(label_type)
   chk::chk_chr(phenotype_class)
   if (!is.null(sc_batch_col)) {
