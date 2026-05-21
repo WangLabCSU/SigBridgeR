411bc0a1c1b5270042200e5bf3a79f5237016ca3
Exceret <15364051195@163.com>
2026-05-21 00:41:21 -0400

    fix checking: add qs2 dependency and remove unused params

--- R/21a-ScissorScreen.R
+++ R/21a-ScissorScreen.R
@@ -27,8 +27,6 @@
 #'      FDR_cutoff = 0.05,
 #'      bootstrap_n = 100L
 #'    ),
-#'    path2load_scissor_cache = NULL,
-#'    path2save_scissor_inputs = "Scissor_inputs.RData",
 #'    ...
 #' )
 #'
