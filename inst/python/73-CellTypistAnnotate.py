7c8e18877203221710de64d35d8916878f138643
Exceret <15364051195@163.com>
2026-07-18 11:20:43 +0800

    proj update

--- inst/python/73-CellTypistAnnotate.py
+++ inst/python/73-CellTypistAnnotate.py
@@ -101,7 +101,8 @@ def filter_args_4_func(input_dict: dict[str, Any], func: Callable) -> dict[str, 
     return {k: v for k, v in input_dict.items() if k in func_params}
 
 
-def main() -> [pd.DataFrame]:
+# pyrefly: ignore [invalid-annotation]
+def main() -> [pd.DataFrame, pd.DataFrame, pd.DataFrame]:
     # * recept them from R script
     adata: AnnData = globals().get("adata")
     model: str = globals().get("model")
