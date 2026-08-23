0891ed3f465c82be53f48a702cab276b189cc0df
Exceret <15364051195@163.com>
2026-08-22 22:53:59 -0400

    feat(SigBridgeR): add merge operator for ScreenMethodResult

--- R/05-DollarProperty.R
+++ R/05-DollarProperty.R
@@ -17,3 +17,16 @@ NULL
 `[[.SigBridgeR::SigBridgeRBase` <- function(object, name) {
   prop(object = object, name = name)
 }
+
+#' Merge two ScreenMethodResult objects
+#' @param x A ScreenMethodResult object as main receiver
+#' @param y A ScreenMethodResult object as main donor
+#' @return A Seurat Object
+#' @rawNamespace S3method("+","SigBridgeR::ScreenMethodResult")
+#' @export
+`+.SigBridgeR::ScreenMethodResult` <- function(x, y) {
+  if (!S7_inherits(y, "ScreenMethodResult")) {
+    Abort("`y` must be a {.cls ScreenMethodResult} object")
+  }
+  MergeResult(x@scRNA_data, y@scRNA_data)
+}
--- inst/conda/README.md
+++ inst/conda/README.md
@@ -1,3 +1,5 @@
 # Conda Environment Config
 
-`SIDISH_*_environment.yml` were deprecated in SigBridgeR v3.8.1 due to R binding updates, left here for reference. 
+`SIDISH_*_environment.yml` were deprecated in SigBridgeR v3.8.1 due to R binding updates, left here for reference.
+
+`celltypist_environment.yml` was deprecated in SigBridgeR v4.0.0, left here for reference. 
